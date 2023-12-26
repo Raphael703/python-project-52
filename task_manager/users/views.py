70d07567baf0b6f944b9a2ac1d75169cb7a95cf4
Raphael <raphael.mukha@gmail.com>
2023-12-27 00:10:56 +0300

    Implement CustomUserCreationForm and UserCreateView

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -7,3 +7,7 @@ class UserListView(ListView):
     model = get_user_model()
     template_name = 'users/user_list.html'
     context_object_name = 'users'
+class UserCreateView(CreateView):
+    template_name = 'users/create.html'
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('user_login')
