07e2163d2464df9ebb1a5a29497d3d49e32f18c6
Raphael <raphael.mukha@gmail.com>
2023-12-27 00:14:41 +0300

    Implement UserUpdateView

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DeleteView
+from django.views.generic import ListView, DeleteView, CreateView, UpdateView
+
+from task_manager.users.forms import CustomUserCreationForm
 
 
 class UserListView(ListView):
@@ -11,6 +14,15 @@ class UserCreateView(CreateView):
     template_name = 'users/create.html'
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('user_login')
+
+
+class UserUpdateView(UpdateView):
+    model = get_user_model()
+    form_class = CustomUserCreationForm
+    template_name = 'users/update.html'
+    success_url = reverse_lazy('user_list')
+
+
 class UserDeleteView(DeleteView):
     model = get_user_model()
     template_name = 'users/delete.html'
