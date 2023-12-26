3c332dd009b0033dc7500631c483f31957305590
Raphael <raphael.mukha@gmail.com>
2023-12-27 00:19:41 +0300

    Register UserLoginView in urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from task_manager.views import index
+from task_manager.views import index, UserLoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('login/', UserLoginView.as_view(), name='user_login'),
     path('users/', include('task_manager.users.urls'))
 ]
