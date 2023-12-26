7b26e0bf16ce52e71c2aecfb92f5ca7b8aafd610
Raphael <raphael.mukha@gmail.com>
2023-12-27 00:19:21 +0300

    Implement UserLoginView

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    next_page = reverse_lazy('index')
