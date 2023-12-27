5506d60295e4265e6ca98a27a199e0487702f39a
Raphael <raphael.mukha@gmail.com>
2023-12-27 22:53:34 +0300

    Implement StatusCreateView

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.utils.translation import gettext as _
 
 from task_manager.statuses.models import Status
 
@@ -7,3 +10,13 @@ from task_manager.statuses.models import Status
 class StatusListView(LoginRequiredMixin, ListView):
     model = Status
     template_name = 'statuses/list.html'
+    context_object_name = 'statuses'
+
+
+class StatusCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
+    model = Status
+    template_name = 'statuses/create.html'
+    fields = ['name']
+    success_url = reverse_lazy('statuses_list')
+    success_message = _('The status has been successfully created')
+
