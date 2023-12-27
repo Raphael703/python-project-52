176b574b534b96e0e48e36fac08d7df596b6611d
Raphael <raphael.mukha@gmail.com>
2023-12-27 21:10:57 +0300

    Expand UserLoginView by SuccessMessageMixin and write success_message using gettext

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,15 +1,18 @@
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.utils.translation import gettext as _
 
 
 def index(request):
     return render(request, 'index.html')
 
 
-class UserLoginView(LoginView):
+class UserLoginView(SuccessMessageMixin, LoginView):
     template_name = 'users/login.html'
     next_page = reverse_lazy('index')
+    success_message = _('You are logged in')
 
 
 class UserLogoutView(LogoutView):
