85602838622492dec796c997519d50308cf26edd
Raphael <raphael.mukha@gmail.com>
2023-12-27 23:04:15 +0300

    Implement StatusDeleteView

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from task_manager.statuses.models import Status
 
@@ -27,3 +27,10 @@ class StatusUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     template_name = 'statuses/update.html'
     success_url = reverse_lazy('statuses_list')
     success_message = _('The status has been successfully changed')
+
+
+class StatusDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+    model = Status
+    template_name = 'statuses/delete.html'
+    success_url = reverse_lazy('statuses_list')
+    success_message = _('The status has been successfully deleted')
