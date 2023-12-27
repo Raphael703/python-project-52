68142e1fea7caf34e82a1095d17b002b6014515f
Raphael <raphael.mukha@gmail.com>
2023-12-27 23:04:30 +0300

    Add StatusDeleteView to urlpatterns

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import StatusListView, StatusCreateView, StatusUpdateView
+
+from .views import StatusListView, StatusCreateView, \
+    StatusUpdateView, StatusDeleteView
 
 urlpatterns = [
     path('', StatusListView.as_view(), name='statuses_list'),
     path('create/', StatusCreateView.as_view(), name='status_create'),
     path('update/<int:pk>', StatusUpdateView.as_view(), name='status_update'),
+    path('delete/<int:pk>', StatusDeleteView.as_view(), name='status_delete'),
 ]
