0c2d7b359a2ff8c67df79f65a666aa2183902901
Raphael <raphael.mukha@gmail.com>
2023-12-28 15:18:59 +0300

    Add TaskCreateView to tasks.urls

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from task_manager.tasks.views import TaskListView
+from task_manager.tasks.views import TaskListView, TaskCreateView
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='tasks_list'),
+    path('create/', TaskCreateView.as_view(), name='task_create'),
 ]
