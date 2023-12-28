49f2619c349ba1337857f8b70282aabb6cf8396e
Raphael <raphael.mukha@gmail.com>
2023-12-28 18:39:29 +0300

    Update CRUD tests for Status by TestStatusSetUpMixin

--- task_manager/statuses/tests.py
+++ task_manager/statuses/tests.py
@@ -1,10 +1,10 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import reverse
 from django.test import TestCase
 
 from task_manager.statuses.models import Status
-from task_manager.users.tests import create_user
 
 test_user_data = {
     'username': 'Abraham',
@@ -16,11 +16,17 @@ def create_status(name):
     return Status.objects.create(name=name)
 
 
-class TestStatusListView(TestCase):
+class TestStatusSetUpMixin:
     def setUp(self):
-        self.user = create_user(test_user_data)
-        self.client.login(
-            username=self.user.username, password=test_user_data['password'])
+        self.user = get_user_model().objects.create_user(
+            username='Abraham', password='qwer1234qwer1234')
+        self.client.login(username=self.user.username,
+                          password=test_user_data['password'])
+
+
+class TestStatusListView(TestStatusSetUpMixin, TestCase):
+    def setUp(self):
+        super().setUp()
         self.url = reverse('statuses_list')
 
     def test_status_list_view(self):
@@ -33,11 +39,9 @@ class TestStatusListView(TestCase):
         self.assertRedirects(response, settings.LOGIN_URL)
 
 
-class TestStatusCreateView(TestCase):
+class TestStatusCreateView(TestStatusSetUpMixin, TestCase):
     def setUp(self):
-        self.user = create_user(test_user_data)
-        self.client.login(
-            username=self.user.username, password=test_user_data['password'])
+        super().setUp()
         self.url = reverse('status_create')
 
     def test_status_create_view_get(self):
@@ -67,12 +71,10 @@ class TestStatusCreateView(TestCase):
             Status.objects.get(name=test_status_name)
 
 
-class TestStatusUpdateView(TestCase):
+class TestStatusUpdateView(TestStatusSetUpMixin, TestCase):
     def setUp(self):
-        self.test_status = create_status('Test status to update')
-        self.user = create_user(test_user_data)
-        self.client.login(
-            username=self.user.username, password=test_user_data['password'])
+        super().setUp()
+        self.test_status = create_status(name='Test status to update')
         self.url = reverse('status_update', kwargs={'pk': self.test_status.pk})
 
     def test_status_update_view_get(self):
@@ -84,8 +86,8 @@ class TestStatusUpdateView(TestCase):
         response = self.client.post(self.url, {'name': test_status_name})
         self.assertRedirects(response, reverse('statuses_list'))
 
-        status = Status.objects.get(name=test_status_name)
-        self.assertEqual(status.name, test_status_name)
+        self.test_status.refresh_from_db()
+        self.assertEqual(self.test_status.name, test_status_name)
 
     def test_status_update_view_get_not_logged_in(self):
         self.client.logout()
@@ -102,12 +104,10 @@ class TestStatusUpdateView(TestCase):
             Status.objects.get(name=test_status_name)
 
 
-class TestStatusDeleteView(TestCase):
+class TestStatusDeleteView(TestStatusSetUpMixin, TestCase):
     def setUp(self):
+        super().setUp()
         self.test_status = create_status('Test status to delete')
-        self.user = create_user(test_user_data)
-        self.client.login(
-            username=self.user.username, password=test_user_data['password'])
         self.url = reverse('status_delete', kwargs={'pk': self.test_status.pk})
 
     def test_status_delete_view_get(self):
