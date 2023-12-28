117376c488a1ed6e882af083b92dc37be8f43145
Raphael <raphael.mukha@gmail.com>
2023-12-28 15:30:26 +0300

    Add TaskDeleteView to tasks.urls

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from task_manager.tasks.views import TaskListView, TaskCreateView, TaskUpdateView
+from task_manager.tasks.views import TaskListView, TaskCreateView, \
+    TaskUpdateView, TaskDeleteView
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='tasks_list'),
     path('create/', TaskCreateView.as_view(), name='task_create'),
     path('<int:pk>/update/', TaskUpdateView.as_view(), name='task_update'),
+    path('<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
 ]
