9440f3c9bca2e99b245a794b80d43b16ca0b7ecb
Raphael <raphael.mukha@gmail.com>
2023-12-28 13:50:28 +0300

    Implement TaskListView

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from task_manager.mixins import CustomLoginRequiredMixin
+from task_manager.tasks.models import Task
+class TaskListView(CustomLoginRequiredMixin, ListView):
+    model = Task
+    template_name = 'tasks/list.html'
+    context_object_name = 'tasks'
 
 # Create your views here.
