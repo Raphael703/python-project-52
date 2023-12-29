a684ebf9c2a20a0930e57891a52581be6ca2c3b0
Raphael <raphael.mukha@gmail.com>
2023-12-29 17:43:35 +0300

    Add message after user logged out

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
@@ -15,5 +16,9 @@ class UserLoginView(SuccessMessageMixin, LoginView):
     success_message = _('You are logged in')
 
 
-class UserLogoutView(LogoutView):
+class UserLogoutView(SuccessMessageMixin, LogoutView):
     next_page = reverse_lazy('index')
+
+    def dispatch(self, request, *args, **kwargs):
+        messages.info(request, _('You are logged out'))
+        return super().dispatch(request, *args, **kwargs)
