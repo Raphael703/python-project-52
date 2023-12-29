c568983f4833cb6fccabaf3503a8fa5bbaaca865
Raphael <raphael.mukha@gmail.com>
2023-12-29 18:17:30 +0300

    Implement IndexView and add it urlpatterns

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from task_manager.views import index, UserLoginView, UserLogoutView
+from task_manager.views import IndexView, UserLoginView, UserLogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', IndexView.as_view(), name='index'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('users/', include('task_manager.users.urls')),
