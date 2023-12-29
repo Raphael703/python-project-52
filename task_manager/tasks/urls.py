63898920775d9c5d75abc4f7813cdfacb13e3084
Raphael <raphael.mukha@gmail.com>
2023-12-29 14:12:06 +0300

    Rename TaskFilterView in tasks/urls.py

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from task_manager.tasks.views import TaskListView, TaskCreateView, \
+from task_manager.tasks.views import TaskFilterView, TaskCreateView, \
     TaskUpdateView, TaskDeleteView, TaskDetailView
 
 urlpatterns = [
-    path('', TaskListView.as_view(), name='tasks_list'),
+    path('', TaskFilterView.as_view(), name='tasks_list'),
     path('<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
     path('create/', TaskCreateView.as_view(), name='task_create'),
     path('<int:pk>/update/', TaskUpdateView.as_view(), name='task_update'),
