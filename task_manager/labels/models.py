144c1631ed3136fd9a312e4c4fea335e71880f49
Raphael <raphael.mukha@gmail.com>
2023-12-29 11:19:48 +0300

    Create model Label and make initial migration

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Label(models.Model):
+    name = models.CharField(_('name'), max_length=128, unique=True)
+    created_at = models.DateTimeField(
+        _('created datetime'), default=timezone.now
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _('Label')
+        verbose_name_plural = _('Labels')
