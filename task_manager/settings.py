202d0f6c0a77c860dd89b920b4bb7aea6ac5d4a5
Raphael <raphael.mukha@gmail.com>
2023-12-29 22:37:15 +0300

    Add setting for SQLite

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -77,6 +77,12 @@ DATABASES = {
     )
 }
 
+if os.getenv('DB_ENGINE') == 'SQLite':
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
