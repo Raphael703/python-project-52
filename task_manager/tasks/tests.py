454e60e0471e01b92ac94c0e097e387b99e51ad2
Raphael <raphael.mukha@gmail.com>
2023-12-30 21:56:17 +0300

    Refactor tasks/tests.py by  SetUpTestDataTaskMixin and restructure setUpTestData in TestClasses

--- task_manager/tasks/tests.py
+++ task_manager/tasks/tests.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import reverse
 from django.test import TestCase
 
@@ -9,17 +10,22 @@ from task_manager.tasks.models import Task
 from task_manager.tests import SetUpLoggedUserMixin
 
 
-class TestTaskDetailView(SetUpLoggedUserMixin, TestCase):
+class SetUpTestDataTaskMixin:
     @classmethod
     def setUpTestData(cls):
         cls.test_status = Status.objects.create(name='Test status')
+        cls.other_user = get_user_model().objects.create(username='nelson_mandela',
+                                                         password='qwer1234qwer')
+        cls.test_task = Task.objects.create(name='Test task',
+                                            status=cls.test_status,
+                                            creator=cls.other_user)
 
-    def setUp(self):
-        super().setUp()
-        self.test_task = Task.objects.create(name='Test task',
-                                             status=self.test_status,
-                                             creator=self.logged_user)
-        self.url = reverse('task_detail', kwargs={'pk': self.test_task.pk})
+
+class TestTaskDetailView(SetUpLoggedUserMixin, SetUpTestDataTaskMixin, TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.url = reverse('task_detail', kwargs={'pk': cls.test_task.pk})
 
     def test_task_detail_view_get(self):
         response = self.client.get(self.url)
@@ -31,23 +37,23 @@ class TestTaskDetailView(SetUpLoggedUserMixin, TestCase):
         self.assertRedirects(response, settings.LOGIN_URL)
 
 
-class TestTaskFilterView(SetUpLoggedUserMixin, TestCase):
+class TestTaskFilterView(SetUpLoggedUserMixin, SetUpTestDataTaskMixin, TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.test_status = Status.objects.create(name='Test status')
+        super().setUpTestData()
         cls.other_status = Status.objects.create(name='Other test status')
 
         cls.other_user = get_user_model().objects.create(
             username='alexander_the_great', password='qwer1234qwer1234'
         )
-        cls.other_user_task1 = Task.objects.create(name='Other user task1',
-                                                   status=cls.test_status,
-                                                   creator=cls.other_user)
-        cls.other_user_task2 = Task.objects.create(name='Other user task2',
-                                                   status=cls.other_status,
-                                                   creator=cls.other_user)
 
         cls.url = reverse('tasks_list')
+
+    def setUp(self):
+        super().setUp()
+        self.logged_user_task = Task.objects.create(name='Logged user task',
+                                                    status=self.other_status,
+                                                    creator=self.logged_user)
 
     def test_task_filter_view_get(self):
         response = self.client.get(self.url)
@@ -56,50 +62,47 @@ class TestTaskFilterView(SetUpLoggedUserMixin, TestCase):
         self.assertQuerysetEqual(list(response.context['tasks']), Task.objects.all())
 
     def test_task_filter_view_get_by_status(self):
-        filter_params_test_status = {'status': self.test_status.pk}
-        response = self.client.get(self.url, filter_params_test_status)
+        filter_param_test_status = {'status': self.test_status.pk}
+        response = self.client.get(self.url, filter_param_test_status)
         self.assertEqual(response.status_code, 200)
 
         self.assertQuerysetEqual(
             response.context['tasks'],
-            Task.objects.filter(**filter_params_test_status)
+            Task.objects.filter(**filter_param_test_status)
         )
 
     def test_task_filter_view_get_by_executor(self):
-        self.other_user_task1.executor = self.logged_user
+        self.test_task.executor = self.logged_user
+        self.test_task.save()
 
-        filter_params_executor = {'status': self.logged_user.pk}
-        response = self.client.get(self.url, filter_params_executor)
+        filter_param_executor = {'executor': self.logged_user.pk}
+        response = self.client.get(self.url, filter_param_executor)
         self.assertEqual(response.status_code, 200)
 
         self.assertQuerysetEqual(
             response.context['tasks'],
-            Task.objects.filter(**filter_params_executor)
+            Task.objects.filter(**filter_param_executor)
         )
 
     def test_task_filter_view_get_by_label(self):
-        label = Label.objects.create(name='Test label')
-        self.other_user_task1.labels.set([label])
+        test_label = Label.objects.create(name='Test label')
+        self.test_task.labels.set([test_label])
 
-        filter_params_label = {'status': label.pk}
-        response = self.client.get(self.url, filter_params_label)
+        filter_param_label = {'label': test_label.pk}
+        response = self.client.get(self.url, filter_param_label)
         self.assertEqual(response.status_code, 200)
 
         self.assertQuerysetEqual(
             response.context['tasks'],
-            Task.objects.filter(**filter_params_label)
+            Task.objects.filter(labels__in=[test_label])
         )
 
     def test_task_filter_view_get_by_own_tasks(self):
-        filter_params_label = {'own_tasks': 'on'}
-
-        own_task = Task.objects.create(name='Own task',
-                                       status=self.test_status,
-                                       creator=self.logged_user)
-        response = self.client.get(self.url, filter_params_label)
+        filter_param_own_tasks = {'own_tasks': 'on'}
+        response = self.client.get(self.url, filter_param_own_tasks)
         self.assertEqual(response.status_code, 200)
 
-        self.assertQuerysetEqual(response.context['tasks'], [own_task])
+        self.assertQuerysetEqual(response.context['tasks'], [self.logged_user_task])
 
     def test_status_filter_view_get_not_logged_in(self):
         self.client.logout()
@@ -107,19 +110,16 @@ class TestTaskFilterView(SetUpLoggedUserMixin, TestCase):
         self.assertRedirects(response, settings.LOGIN_URL)
 
 
-class TestTaskCreateView(SetUpLoggedUserMixin, TestCase):
+class TestTaskCreateView(SetUpLoggedUserMixin, SetUpTestDataTaskMixin, TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.test_status = Status.objects.create(name='Test status')
-
-    def setUp(self):
-        super().setUp()
-        self.data_to_create_task = {
+        super().setUpTestData()
+        cls.data_to_create_task = {
             'name': 'Test task to create',
-            'status': self.test_status.pk,
-            'creator': self.logged_user.pk
+            'status': cls.test_status.pk,
+            'creator': cls.other_user.pk
         }
-        self.url = reverse('task_create')
+        cls.url = reverse('task_create')
 
     def test_task_create_view_get(self):
         response = self.client.get(self.url)
@@ -144,22 +144,16 @@ class TestTaskCreateView(SetUpLoggedUserMixin, TestCase):
         self.assertFalse(Task.objects.filter(name=self.data_to_create_task).exists())
 
 
-class TestTaskUpdateView(SetUpLoggedUserMixin, TestCase):
+class TestTaskUpdateView(SetUpLoggedUserMixin, SetUpTestDataTaskMixin, TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.test_status = Status.objects.create(name='Test status')
+        super().setUpTestData()
         cls.data_to_update_task = {
             'name': 'Test task to update',
             'status': cls.test_status.pk,
             'description': 'Some description'
         }
-
-    def setUp(self):
-        super().setUp()
-        self.test_task = Task.objects.create(name='Test task',
-                                             status=self.test_status,
-                                             creator=self.logged_user)
-        self.url = reverse('task_update', kwargs={'pk': self.test_task.pk})
+        cls.url = reverse('task_update', kwargs={'pk': cls.test_task.pk})
 
     def test_task_update_view_get(self):
         response = self.client.get(self.url)
@@ -171,8 +165,7 @@ class TestTaskUpdateView(SetUpLoggedUserMixin, TestCase):
 
         self.test_task.refresh_from_db()
         self.assertEqual(self.test_task.name, self.data_to_update_task['name'])
-        self.assertEqual(self.test_task.description,
-                         self.data_to_update_task['description'])
+        self.assertEqual(self.test_task.description, self.data_to_update_task['description'])
 
     def test_task_update_view_get_not_logged_in(self):
         self.client.logout()
@@ -185,55 +178,50 @@ class TestTaskUpdateView(SetUpLoggedUserMixin, TestCase):
         self.assertRedirects(response, settings.LOGIN_URL)
 
         self.test_task.refresh_from_db()
-        self.assertNotEqual(self.test_task.name,
-                            self.data_to_update_task['name'])
+        self.assertNotEqual(self.test_task.name, self.data_to_update_task['name'])
 
 
-class TestTaskDeleteView(SetUpLoggedUserMixin, TestCase):
+class TestTaskDeleteView(SetUpLoggedUserMixin, SetUpTestDataTaskMixin, TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.test_status = Status.objects.create(name='Test status')
-        cls.other_user = get_user_model().objects.create(
-            username='alexander_the_great', password='qwer1234qwer1234'
-        )
-        cls.other_user_task = Task.objects.create(name='Other user task',
-                                                  status=cls.test_status,
-                                                  creator=cls.other_user)
+        super().setUpTestData()
+        cls.url_other = reverse('task_delete', kwargs={'pk': cls.test_task.pk})
 
     def setUp(self):
         super().setUp()
-        self.test_task = Task.objects.create(name='Test task',
-                                             status=self.test_status,
-                                             creator=self.logged_user)
-        self.url = reverse('task_delete', kwargs={'pk': self.test_task.pk})
+        self.logged_user_task = Task.objects.create(name='Logged user task',
+                                                    status=self.test_status,
+                                                    creator=self.logged_user)
+        self.url_own = reverse('task_delete', kwargs={'pk': self.logged_user_task.pk})
 
     def test_task_delete_view_get(self):
-        response = self.client.get(self.url)
+        response = self.client.get(self.url_own)
         self.assertEqual(response.status_code, 200)
 
     def test_task_delete_view_post(self):
-        response = self.client.post(self.url)
+        response = self.client.post(self.url_own)
         self.assertRedirects(response, reverse('tasks_list'))
 
-        self.assertFalse(Task.objects.filter(pk=self.test_task.pk).exists())
+        with self.assertRaises(ObjectDoesNotExist):
+            Task.objects.get(pk=self.logged_user_task.pk)
 
     def test_task_delete_view_post_other_user(self):
-        response = self.client.post(self.url)
+        response = self.client.post(self.url_other)
         self.assertRedirects(response, reverse('tasks_list'))
 
         self.assertTrue(
-            Task.objects.filter(pk=self.other_user_task.pk).exists(),
+            Task.objects.filter(pk=self.test_task.pk).exists(),
             'Task should not be deleted by other user'
         )
 
     def test_task_delete_view_get_not_logged_in(self):
         self.client.logout()
-        response = self.client.get(self.url)
+        response = self.client.get(self.url_own)
         self.assertRedirects(response, settings.LOGIN_URL)
 
     def test_task_delete_view_post_not_logged_in(self):
         self.client.logout()
-        response = self.client.post(self.url)
+        response = self.client.post(self.url_own)
         self.assertRedirects(response, settings.LOGIN_URL)
 
-        self.assertTrue(Task.objects.filter(pk=self.test_task.pk).exists())
+        self.assertTrue(Task.objects.filter(pk=self.logged_user_task.pk).exists())
