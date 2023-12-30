bf96a04cd4a9ae49f93659b7263e54ad8419f1d5
Raphael <raphael.mukha@gmail.com>
2023-12-30 19:08:10 +0300

    Refactor CustomLoginRequiredMixin

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -2,9 +2,10 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.utils.translation import gettext as _
 
 
 class CustomLoginRequiredMixin(LoginRequiredMixin):
     def handle_no_permission(self):
-        messages.error(self.request, "You are not logged in! Please log in.")
+        messages.error(self.request, _('You are not logged in! Please log in.'))
         return redirect(reverse('login'))
